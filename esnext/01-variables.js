501c83c1ea5273e92554dde389d1c24f4b2c160f
Matteo Antony Mistretta <m.mistretta@reply.it>
2025-03-04 15:19:41 +0100

    Add ESLint

--- esnext/01-variables.js
+++ esnext/01-variables.js
@@ -1,7 +1,42 @@
+// hoisting
+// var a;
+
 console.log("Hello world!");
 
-var a = 1;
+// var a = 1;
 
 let b = 2;
 const c = 3;
-c = 4;
+// c = 4;
+
+// block scoping
+{
+  var a = 1;
+  let b = 2;
+}
+
+function func() {
+  a = 1; // eslint-disable-line no-magic-numbers
+  var a = 1;
+  let b = 2;
+}
+
+// global namespace pollution
+let num = 1;
+console.log(typeof num);
+let obj = {};
+console.log(typeof obj);
+let arr = [];
+console.log(typeof arr, Array.isArray(arr));
+let nil = null;
+console.log(typeof nil, nil == null);
+let notANumber = NaN;
+console.log(typeof notANumber, Number.isNaN(notANumber));
+let infinity = Infinity;
+console.log(typeof infinity, Number.isFinite(infinity));
+
+const COUNTDOWN = 10;
+
+for (let i = 0; i < COUNTDOWN; i++) {
+  console.log(i);
+}
